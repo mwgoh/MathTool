7afdda59e5c0dff12a3a15a6f92502889285c785
codeit <gmw0924@gmail.com>
2023-09-04 13:17:23 +0900

    calculator.py 테슽

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+#테스트 추가
+#계산기
 def add(a,b):
 	return a+b;
 
--- test.py
+++ test.py
@@ -3,3 +3,10 @@ git init
 git config user.name "gmw"
 
 git config user.email "gmw@gmail.com"
+
+git status
+
+git commit -m "commit 내용 적기"
+
+git add . 현재 프로젝트 디렉토리 내에서 변경사항을 
+          stage 에 추가
